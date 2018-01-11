7fe1780f9ced997b2bb4b3341822bbb951344a20
Pijar Aji Pratama <pijars@Pijars-MacBook-Pro.local>
2018-01-12 05:45:27 +0700

    artisan console

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,7 +1,11 @@
 <?php
+
 namespace WpPluginner\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
 use WpPluginner\Framework\Foundation\Console\Kernel as ConsoleKernel;
+
 class Kernel extends ConsoleKernel
 {
-  protected $commands = [];
+
 }
